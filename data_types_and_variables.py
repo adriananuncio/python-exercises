a93de15d1335244eb7d30779c5f3ed5fc36428f5
Adriana Nuncio <adrianapnuncio@gmail.com>
2023-03-02 12:44:06 -0600

    added file

---
+++ data_types_and_variables.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+type(99.9)
+
+
+# In[2]:
+
+
+type("False")
+
+
+# In[3]:
+
+
+type(False)
+
+
+# In[4]:
+
+
+type('0')
+
+
+# In[5]:
+
+
+type(True)
+
+
+# In[6]:
+
+
+type('True')
+
+
+# In[7]:
+
+
+type([{}])
+
+
+# In[8]:
+
+
+type({'a': []})
+
+
+# ### 2. What data type would best represent the following?
+# 
+# A term or phrase typed into a search box 
+#     
+#     str
+# 
+# Whether or not a user is logged in
+#     
+#     bool
+# 
+# A discount amount to apply to a user's shopping cart
+#     
+#     float
+# 
+# Whether or not a coupon code is valid
+#     
+#     bool
+# 
+# An email address typed into a registration form
+#     
+#     str
+# 
+# The price of a product
+#     
+#     float
+# 
+# The email addresses collected from a registration form
+#     
+#     list
+# 
+# Information about applicants to Codeup's data science program
+#     
+#     dict
+
+# In[9]:
+
+
+'1' + 2
+
+
+# In[10]:
+
+
+6 % 4
+
+
+# In[11]:
+
+
+type(6 % 4)
+
+
+# In[12]:
+
+
+type(type(6 % 4))
+
+
+# In[13]:
+
+
+'3 + 4 is ' + 3 + 4
+
+
+# In[14]:
+
+
+0 < 0
+
+
+# In[15]:
+
+
+'False' == False
+
+
+# In[16]:
+
+
+True == 'True'
+
+
+# In[17]:
+
+
+5 >= -5
+
+
+# In[18]:
+
+
+True or "42"
+
+
+# In[19]:
+
+
+6 % 5
+
+
+# In[20]:
+
+
+5 < 4 and 1 == 1
+
+
+# In[21]:
+
+
+'codeup' == 'codeup' and 'codeup' == 'Codeup'
+
+
+# In[22]:
+
+
+4 >= 0 and 1 !== '1'
+
+
+# In[23]:
+
+
+6 % 3 == 0
+
+
+# In[24]:
+
+
+5 % 2 != 0
+
+
+# In[25]:
+
+
+[1] + 2
+
+
+# In[26]:
+
+
+[1] + [2]
+
+
+# In[27]:
+
+
+[1] * 2
+
+
+# In[28]:
+
+
+[1] * [2]
+
+
+# In[29]:
+
+
+[] + [] == []
+
+
+# In[30]:
+
+
+{} + {}
+
